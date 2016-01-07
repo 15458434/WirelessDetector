783106c7929ff5bc72ac1f7e7796a494ed73024d
Mark Cornelisse <info@markcornelisse.nl>
2016-01-07 18:17:08 +0100

    Initial Commit

---
+++ WirelessDetection/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WirelessDetection
+//
+//  Created by Mark Cornelisse on 07/01/16.
+//  Copyright © 2016 Over de muur producties. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
