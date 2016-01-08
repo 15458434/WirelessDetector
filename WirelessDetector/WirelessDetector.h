3810cda8225cd66c66d58e2a717056776b092ecd
Mark Cornelisse <info@markcornelisse.nl>
2016-01-08 12:11:06 +0100

    - First version all files.

---
+++ WirelessDetector/WirelessDetector.h
@@ -0,0 +1,19 @@
+//
+//  WirelessDetector.h
+//  WirelessDetector
+//
+//  Created by Mark Cornelisse on 07/01/16.
+//  Copyright © 2016 Over de muur producties. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WirelessDetector.
+FOUNDATION_EXPORT double WirelessDetectorVersionNumber;
+
+//! Project version string for WirelessDetector.
+FOUNDATION_EXPORT const unsigned char WirelessDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WirelessDetector/PublicHeader.h>
+
+
